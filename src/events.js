c2c9cd2e1e0112627a18c72772c85a5440b4ade2
Greg Hanson <greg@greghanson.co.uk>
2022-12-27 11:38:24 +0000

    Added POST /gettodaysplaycount

--- src/events.js
+++ src/events.js
@@ -1,5 +1,6 @@
 ﻿const express = require('express');
 const bodyParser = require("body-parser");
+const {DATETIME, DATE, DATETIME2} = require("mysql/lib/protocol/constants/types");
 
 function createRouter(db) {
     const router = express.Router();
@@ -88,7 +89,28 @@ function createRouter(db) {
             }
         );
     });
-    
+
+    router.post('/gettodaysplaycount', function (req, res, next) {
+
+        const email = req.body.email;
+        let count = 0;
+
+        db.query(
+            'SELECT ID, requested FROM requests WHERE username LIKE ?',
+            [email],
+            (error, results) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({status: 'error'});
+                } else {
+                    res.status(200).json(getRequestCount(results));
+                }
+            }
+        );
+    });
+
+   
+
     router.get('/getallsongs', (req,res) =>{
         db.query(
             'SELECT ID, artist, title FROM songs',
@@ -135,7 +157,7 @@ function createRouter(db) {
                                     console.error(error);
                                     res.status(500).json({status: 'error'});
                                 } else {
-                                    res.status(200).json({status: 'ok'});
+                                    res.status(200).json({status: 'user added to DB'});
                                 }
                             }
                         );
@@ -147,6 +169,68 @@ function createRouter(db) {
             }
         );
     });
+    
+    
+    //HELPER METHODS
+    function wasToday(incoming) {
+
+        let today = new Date()
+        let t_dd = String(today.getDate()).padStart(2, '0');
+        let t_mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0 - stupid
+        let t_yyyy = today.getFullYear();
+        let t_result = t_dd + "/" + t_mm + "/" + t_yyyy
+
+        let r_string = String(incoming.requested);
+
+        let r_dd = r_string.substring(8,10);
+        let r_mm = getMonth(r_string.substring(4,7));
+        let r_yyyy = r_string.substring(11,15);
+        let r_result = r_dd + "/" + r_mm + "/" + r_yyyy
+
+        return t_result === r_result;
+
+    }
+
+    function getMonth(month) {
+        switch (month) {
+            case "Jan":
+                return "01";
+            case "Feb":
+                return "02";
+            case "Mar":
+                return "03";
+            case "Apr":
+                return "04";
+            case "May":
+                return "05";
+            case "Jun":
+                return "06";
+            case "Jul":
+                return "07";
+            case "Aug":
+                return "08";
+            case "Sep":
+                return "09";
+            case "Oct":
+                return "10";
+            case "Nov":
+                return "11";
+            case "Dec":
+                return "12";
+        }
+    }
+    
+    function getRequestCount(results){
+        
+        let count = 0;
+        
+        results.forEach(element => {
+            if(wasToday(element)){
+                count++;
+            }
+        })
+        return count;
+    }
      
 
     return router;
